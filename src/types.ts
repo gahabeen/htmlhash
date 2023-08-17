95fc352899cbf84da02daa35fbf81cd84eeb64e4
Gabin Desserprit <desserprit.gabin@gmail.com>
2023-08-17 11:35:02 +0200

    up

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,3 @@
-import { z } from 'zod';
-
 export type Id = string;
 export type Tag = string;
 export type Attribute = string;
@@ -8,9 +6,3 @@ export type Count = number;
 
 export type HashVector = number[];
 export type AnyHashWithValues = { hash: HashVector, squarred?: number }
-
-export const ClusteringOptions = z.object({
-    scoreTreshold: z.number().default(0.745),
-}).default({});
-
-export type ClusteringOptions = z.infer<typeof ClusteringOptions>;
